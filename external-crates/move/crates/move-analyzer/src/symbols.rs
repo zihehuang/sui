7e0e5f0c3813a6d1d3b4d6f07357c1f007a51464
Cameron Swords <cameron.swords@mystenlabs.com>
2024-02-21 14:24:41 -0800

    toward rebase

--- external-crates/move/crates/move-analyzer/src/symbols.rs
+++ external-crates/move/crates/move-analyzer/src/symbols.rs
@@ -87,7 +87,7 @@ use move_compiler::{
     editions::Flavor,
     expansion::ast::{self as E, Fields, ModuleIdent, ModuleIdent_, Value, Value_, Visibility},
     naming::ast::{StructDefinition, StructFields, TParam, Type, TypeName_, Type_, UseFuns},
-    parser::ast::{self as P, StructName},
+    parser::ast::{self as P, DatatypeName},
     shared::{Identifier, Name},
     typing::ast::{
         BuiltinFunction_, Exp, ExpListItem, Function, FunctionBody_, LValue, LValueList, LValue_,
@@ -1495,10 +1495,25 @@ impl<'a> ParsingSymbolicator<'a> {
                     self.type_symbols(&fun.signature.return_type);
                 }
                 MM::Struct(sdef) => match &sdef.fields {
-                    P::StructFields::Defined(v) => v.iter().for_each(|(_, t)| self.type_symbols(t)),
+                    P::StructFields::Named(v) => v.iter().for_each(|(_, t)| self.type_symbols(t)),
                     P::StructFields::Positional(v) => v.iter().for_each(|t| self.type_symbols(t)),
                     P::StructFields::Native(_) => (),
                 },
+                MM::Enum(edef) => {
+                    let P::EnumDefinition { variants, .. } = edef;
+                    for variant in variants {
+                        let P::VariantDefinition { fields, .. } = variant;
+                        match fields {
+                            P::VariantFields::Named(v) => {
+                                v.iter().for_each(|(_, t)| self.type_symbols(t))
+                            }
+                            P::VariantFields::Positional(v) => {
+                                v.iter().for_each(|t| self.type_symbols(t))
+                            }
+                            P::VariantFields::Empty => (),
+                        }
+                    }
+                }
                 MM::Use(use_decl) => self.use_decl_symbols(use_decl),
                 MM::Friend(fdecl) => self.chain_symbols(&fdecl.friend),
                 MM::Constant(c) => {
@@ -1841,6 +1856,7 @@ impl<'a> ParsingSymbolicator<'a> {
                 }
             }
             NA::Three(..) => None,
+            NA::Four(..) => None,
         };
         let Some(n) = no else {
             return;
@@ -2106,6 +2122,9 @@ impl<'a> TypingSymbolicator<'a> {
                 self.unpack_symbols(define, ident, name, tparams, fields, scope);
             }
             LValue_::Ignore => (),
+            LValue_::UnpackVariant(..) | LValue_::BorrowUnpackVariant(..) => {
+                debug_assert!(false, "Enums are not supported by move analyzser.");
+            }
         }
     }
 
@@ -2114,7 +2133,7 @@ impl<'a> TypingSymbolicator<'a> {
         &mut self,
         define: bool,
         ident: &ModuleIdent,
-        name: &StructName,
+        name: &DatatypeName,
         tparams: &Vec<Type>,
         fields: &Fields<(Type, LValue)>,
         scope: &mut OrdMap<Symbol, LocalDef>,
@@ -2323,7 +2342,7 @@ impl<'a> TypingSymbolicator<'a> {
     fn pack_symbols(
         &mut self,
         ident: &ModuleIdent,
-        name: &StructName,
+        name: &DatatypeName,
         tparams: &Vec<Type>,
         fields: &Fields<(Type, Exp)>,
         scope: &mut OrdMap<Symbol, LocalDef>,
